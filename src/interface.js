ffebdbf287f31652f3abcbd9f4a9172773b64fd3
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-07-18 05:57:50 -0700

    Fix errors

--- src/interface.js
+++ src/interface.js
@@ -10,37 +10,37 @@ import {
 import { addToLocalStorage } from './localstorage.js';
 // import updateCompletionStatus from './complete.js';
 
-// const form = document.querySelector('.todo--form');
-// const formInput = form.querySelector('input');
-// const taskContainer = document.querySelector('.list--container');
-// const clearBtn = document.querySelector('.clear--btn');
-// const enterIcon = document.querySelector('.fa-arrow-turn-down');
-// const refreshIcon = document.querySelector('.fa-arrows-rotate');
-// function populateTasks() {
-//   taskContainer.innerHTML = '';
-//   const element = tasks.map((task) => {
-//     const { completed } = task;
-//     const li = `<li class="list--item">
-//       <div class="first--flex">
-//           <input type="checkbox" name="" id=${task.index} class="check" ${completed ? 'checked' : ''} >
-//           <div class="desc---container">
-//               <p class="desc ${completed && 'strike--through'}">${task.desc}</p>
-//               <form action="" class="edit--form">
-//                 <input type="text" name="" id="" class="edit--input">
-//               </form>
-//           </div>
-//       </div>
-//       <div class="second--flex">
-//         <i class="fa-solid fa-ellipsis-vertical" data--option=${task.index}></i>
-//         <i class="fa-solid fa-trash-can" data--trash=${task.index}></i>
-//       </div>
-//     </li>`;
-//     return li;
-//   }).join('');
-//   taskContainer.insertAdjacentHTML('beforeend', element);
-//   const optionsIcon = document.querySelectorAll('.fa-ellipsis-vertical');
-//   const deleteIcons = document.querySelectorAll('.fa-trash-can');
-//   const checkBoxes = document.querySelectorAll('.check');
+const form = document.querySelector('.todo--form');
+const formInput = form.querySelector('input');
+const taskContainer = document.querySelector('.list--container');
+const clearBtn = document.querySelector('.clear--btn');
+const enterIcon = document.querySelector('.fa-arrow-turn-down');
+const refreshIcon = document.querySelector('.fa-arrows-rotate');
+function populateTasks() {
+  taskContainer.innerHTML = '';
+  const element = tasks.map((task) => {
+    const { completed } = task;
+    const li = `<li class="list--item">
+      <div class="first--flex">
+          <input type="checkbox" name="" id=${task.index} class="check" ${completed ? 'checked' : ''} >
+          <div class="desc---container">
+              <p class="desc ${completed && 'strike--through'}">${task.desc}</p>
+              <form action="" class="edit--form">
+                <input type="text" name="" id="" class="edit--input">
+              </form>
+          </div>
+      </div>
+      <div class="second--flex">
+        <i class="fa-solid fa-ellipsis-vertical" data--option=${task.index}></i>
+        <i class="fa-solid fa-trash-can" data--trash=${task.index}></i>
+      </div>
+    </li>`;
+    return li;
+  }).join('');
+  taskContainer.insertAdjacentHTML('beforeend', element);
+  const optionsIcon = document.querySelectorAll('.fa-ellipsis-vertical');
+  const deleteIcons = document.querySelectorAll('.fa-trash-can');
+  const checkBoxes = document.querySelectorAll('.check');
 
   optionsIcon.forEach((icon) => {
     icon.addEventListener('click', (e) => {
